1916335077063f86f4ab7544d79dce57f87546f5
Stas SUȘCOV <stas@nerd.ro>
2022-09-02 18:34:54 +0100

    Added veNFT sugar.

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,14 +1,21 @@
 import os
 
-from brownie import accounts, Sugar
+from brownie import accounts, VeSugar, PairsSugar
 
 
 def main():
     account = accounts.load('sugar')
 
-    sugar = Sugar.deploy({'from': account})
-    sugar.setup(
+    psugar = PairsSugar.deploy({'from': account})
+    psugar.setup(
         os.getenv('VOTER_ADDRESS'),
         os.getenv('WRAPPED_BRIBE_FACTORY'),
+        {'from': account}
+    )
+
+    vesugar = VeSugar.deploy({'from': account})
+    vesugar.setup(
+        os.getenv('VOTER_ADDRESS'),
+        os.getenv('REWARDS_DIST_ADDRESS'),
         {'from': account}
     )
