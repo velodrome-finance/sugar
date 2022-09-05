5e2640ecf14bb1cedf790e62d21e470b27174550
Stas SUȘCOV <stas@nerd.ro>
2022-09-06 00:17:59 +0100

    Updated the deployment script.

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -4,18 +4,23 @@ from brownie import accounts, VeSugar, PairsSugar
 
 
 def main():
+    contract_name = str(os.getenv('CONTRACT')).lower()
     account = accounts.load('sugar')
 
-    psugar = PairsSugar.deploy({'from': account})
-    psugar.setup(
-        os.getenv('VOTER_ADDRESS'),
-        os.getenv('WRAPPED_BRIBE_FACTORY'),
-        {'from': account}
-    )
-
-    vesugar = VeSugar.deploy({'from': account})
-    vesugar.setup(
-        os.getenv('VOTER_ADDRESS'),
-        os.getenv('REWARDS_DIST_ADDRESS'),
-        {'from': account}
-    )
+    if 'pairs' in contract_name:
+        psugar = PairsSugar.deploy({'from': account})
+        psugar.setup(
+            os.getenv('VOTER_ADDRESS'),
+            os.getenv('WRAPPED_BRIBE_FACTORY'),
+            {'from': account}
+        )
+    elif 've' in contract_name:
+        vesugar = VeSugar.deploy({'from': account})
+        vesugar.setup(
+            os.getenv('VOTER_ADDRESS'),
+            os.getenv('REWARDS_DIST_ADDRESS'),
+            os.getenv('PAIRS_SUGAR_ADDRESS'),
+            {'from': account}
+        )
+    else:
+        print('Set the `CONTRACT` environment variable to deploy a contract.')
