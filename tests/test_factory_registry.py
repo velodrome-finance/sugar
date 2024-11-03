b1bdbd0088563f00f4c14a5493324ed190a868f5
Stas SUȘCOV <stas@nerd.ro>
2024-11-03 19:36:54 +0000

    FactoryRegistry: refactor init hashes.

--- tests/test_factory_registry.py
+++ tests/test_factory_registry.py
@@ -27,3 +27,11 @@ def test_factoriesToPoolFactory(factory_registry):
 
     result = factory_registry.factoriesToPoolFactory(ADDRESS_ZERO)
     assert ADDRESS_ZERO in result
+
+
+@pytest.mark.skipif(int(CHAIN_ID) in [10, 8453], reason="Only leaf chains")
+def test_initHashToPoolFactory(factory_registry):
+    factories = factory_registry.poolFactories()
+    result = factory_registry.initHashToPoolFactory(factories[0])
+
+    assert str(result) in os.getenv(f'INIT_HASHES_{CHAIN_ID}')
