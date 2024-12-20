434cd695ed5f9612b579cf08cb6e68de2cc3c4b7
Stas SUȘCOV <stas@nerd.ro>
2024-12-20 19:21:17 +0000

    chore: switched to `eth-ape`

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,62 +1,66 @@
 # SPDX-License-Identifier: BUSL-1.1
 import os
 
-from brownie import (
-    accounts, VeSugar, LpSugar, RelaySugar, FactoryRegistry, RewardsSugar
-)
+from ape import project, accounts
 
 
 def main():
-    contract_name = str(os.getenv('CONTRACT')).lower()
-    chain_id = os.getenv('CHAIN_ID')
+    contract_name = str(os.getenv("CONTRACT")).lower()
+    chain_id = os.getenv("CHAIN_ID")
+    publish = os.getenv("PUBLISH", False)
     account = None
 
-    if os.getenv('PROD'):
-        account = accounts.load('sugar')
+    if os.getenv("PROD"):
+        account = accounts.load("sugar")
     elif len(accounts) > 0:
         account = accounts[0]
 
-    if 'lp' in contract_name:
-        LpSugar.deploy(
-            os.getenv(f'VOTER_{chain_id}'),
-            os.getenv(f'REGISTRY_{chain_id}'),
-            os.getenv(f'CONVERTOR_{chain_id}'),
-            os.getenv(f'SLIPSTREAM_HELPER_{chain_id}'),
-            os.getenv(f'ALM_FACTORY_{chain_id}'),
-            {'from': account}
+    if "lp" in contract_name:
+        project.LpSugar.deploy(
+            os.getenv(f"VOTER_{chain_id}"),
+            os.getenv(f"REGISTRY_{chain_id}"),
+            os.getenv(f"CONVERTOR_{chain_id}"),
+            os.getenv(f"SLIPSTREAM_HELPER_{chain_id}"),
+            os.getenv(f"ALM_FACTORY_{chain_id}"),
+            sender=account,
+            publish=publish,
         )
 
-    elif 'rewards' in contract_name:
-        RewardsSugar.deploy(
-            os.getenv(f'VOTER_{chain_id}'),
-            os.getenv(f'REGISTRY_{chain_id}'),
-            os.getenv(f'CONVERTOR_{chain_id}'),
-            {'from': account}
+    elif "rewards" in contract_name:
+        project.RewardsSugar.deploy(
+            os.getenv(f"VOTER_{chain_id}"),
+            os.getenv(f"REGISTRY_{chain_id}"),
+            os.getenv(f"CONVERTOR_{chain_id}"),
+            sender=account,
+            publish=publish,
         )
 
-    elif 've' in contract_name:
-        VeSugar.deploy(
-            os.getenv(f'VOTER_{chain_id}'),
-            os.getenv(f'DIST_{chain_id}'),
-            os.getenv(f'GOVERNOR_{chain_id}'),
-            {'from': account}
+    elif "ve" in contract_name:
+        project.VeSugar.deploy(
+            os.getenv(f"VOTER_{chain_id}"),
+            os.getenv(f"DIST_{chain_id}"),
+            os.getenv(f"GOVERNOR_{chain_id}"),
+            sender=account,
+            publish=publish,
         )
 
-    elif 'relay' in contract_name:
-        RelaySugar.deploy(
-            str(os.getenv(f'RELAY_REGISTRY_ADDRESSES_{chain_id}')).split(','),
-            os.getenv(f'VOTER_{chain_id}'),
-            {'from': account}
+    elif "relay" in contract_name:
+        project.RelaySugar.deploy(
+            str(os.getenv(f"RELAY_REGISTRY_ADDRESSES_{chain_id}")).split(","),
+            os.getenv(f"VOTER_{chain_id}"),
+            sender=account,
+            publish=publish,
         )
 
-    elif 'registry' in contract_name:
-        FactoryRegistry.deploy(
-            str(os.getenv(f'FACTORIES_{chain_id}')).split(','),
-            str(os.getenv(f'REWARDS_FACTORIES_{chain_id}')).split(','),
-            str(os.getenv(f'GAUGE_FACTORIES_{chain_id}')).split(','),
-            str(os.getenv(f'INIT_HASHES_{chain_id}')).split(','),
-            {'from': account}
+    elif "registry" in contract_name:
+        project.FactoryRegistry.deploy(
+            str(os.getenv(f"FACTORIES_{chain_id}")).split(","),
+            str(os.getenv(f"REWARDS_FACTORIES_{chain_id}")).split(","),
+            str(os.getenv(f"GAUGE_FACTORIES_{chain_id}")).split(","),
+            str(os.getenv(f"INIT_HASHES_{chain_id}")).split(","),
+            sender=account,
+            publish=publish,
         )
 
     else:
-        print('Set the `CONTRACT` environment variable to deploy a contract.')
+        print("Set the `CONTRACT` environment variable to deploy a contract.")
--- tests/test_relay_sugar.py
+++ tests/test_relay_sugar.py
@@ -2,43 +2,32 @@
 import os
 import pytest
 
-from collections import namedtuple
 from web3.constants import ADDRESS_ZERO
 
-CHAIN_ID = os.getenv('CHAIN_ID', 10)
+CHAIN_ID = os.getenv("CHAIN_ID", 10)
 
 
 @pytest.fixture
 @pytest.mark.skipif(int(CHAIN_ID) not in [10, 8453], reason="Only root chains")
-def sugar_contract(RelaySugar, accounts):
+def sugar_contract(project, accounts):
     # Since we depend on the rest of the protocol,
     # we just point to an existing deployment
-    yield RelaySugar.at(os.getenv(f'RELAY_SUGAR_ADDRESS_{CHAIN_ID}'))
-
-
-@pytest.fixture
-@pytest.mark.skipif(int(CHAIN_ID) not in [10, 8453], reason="Only root chains")
-def RelayStruct(sugar_contract):
-    method_output = sugar_contract.all.abi['outputs'][0]
-    members = list(map(lambda _e: _e['name'], method_output['components']))
-
-    yield namedtuple('RelayStruct', members)
+    yield project.RelaySugar.at(os.getenv(f"RELAY_SUGAR_ADDRESS_{CHAIN_ID}"))
 
 
 @pytest.mark.skipif(int(CHAIN_ID) not in [10, 8453], reason="Only root chains")
 def test_initial_state(sugar_contract):
-    assert sugar_contract.voter() == os.getenv(f'VOTER_{CHAIN_ID}')
-    assert sugar_contract.registries(0) == \
-        os.getenv(f'RELAY_REGISTRY_ADDRESSES_{CHAIN_ID}').split(',')[0]
+    assert sugar_contract.voter() == os.getenv(f"VOTER_{CHAIN_ID}")
+    assert (
+        sugar_contract.registries(0)
+        == os.getenv(f"RELAY_REGISTRY_ADDRESSES_{CHAIN_ID}").split(",")[0]
+    )
     assert sugar_contract.ve() is not None
     assert sugar_contract.token() is not None
 
 
 @pytest.mark.skipif(int(CHAIN_ID) not in [10, 8453], reason="Only root chains")
-def test_all(sugar_contract, RelayStruct):
-    relays = list(map(
-        lambda _r: RelayStruct(*_r),
-        sugar_contract.all(ADDRESS_ZERO)
-    ))
+def test_all(sugar_contract):
+    relays = sugar_contract.all(ADDRESS_ZERO)
 
     assert len(relays) > 5
